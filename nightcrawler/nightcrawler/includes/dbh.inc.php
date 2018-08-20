d99e78f7c0d43f456c9a20766d30f8390abfae1d
s3482078 <s3482078@student.rmit.edu.au>
2018-08-20 20:17:25 +1000

    Cleaned up folder
    I cleaned up the folder "nightcrawler". Not sure if it works. I've moved all the MVFs into their indivudal folders. I'll comment throughout the files later on, and work on the css and turning the MVFs people handed in into the main website.

--- nightcrawler/nightcrawler/includes/dbh.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$dbServername = "localhost";
-$dbUsername = "root";
-$dbPassname = "";
-$dbName = "nightcrawler";
-
-$conn = mysqli_connect($dbServername, $dbUsername, $dbPassname, $dbName);
